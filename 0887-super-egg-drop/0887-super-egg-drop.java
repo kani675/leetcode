9cea4ace271a7bbd9b0ef5c23da7200e11d82912
Kanishka G <anbu27041982@gmail.com>
2026-06-26 13:48:56 +0530

    Time: 0 ms (100%), Space: 41.9 MB (94.09%) - LeetHub

---
+++ 0887-super-egg-drop/0887-super-egg-drop.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int superEggDrop(int k, int n) {
+        int[] dp = new int[k + 1];
+        int moves = 0;
+        
+        while (dp[k] < n) {
+            moves++;
+            
+            for (int i = k; i > 0; i--) {
+                dp[i] = dp[i - 1] + dp[i] + 1;
+            }
+        }
+        
+        return moves;
+    }
+}
