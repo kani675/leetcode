9c3409263753c2a6d58fe46cb66bcf91b9c045a6
Kanishka G <anbu27041982@gmail.com>
2026-06-26 15:58:18 +0530

    Time: 10 ms (60.71%), Space: 12.4 MB (89.54%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def countAndSay(self, n):
+        result = "1"
+        
+        for _ in range(n - 1):
+            temp = ""
+            count = 1
+            
+            for i in range(1, len(result)):
+                if result[i] == result[i - 1]:
+                    count += 1
+                else:
+                    temp += str(count) + result[i - 1]
+                    count = 1
+            
+            # Add last group
+            temp += str(count) + result[-1]
+            result = temp
+        
+        return result
